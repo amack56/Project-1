2e0c1f20fef7d90d1e73a7b7126b3595591cec64
amack56 <alexmack56@gmail.com>
2021-09-17 20:15:56 -0500

    added dist and top pos graphs

--- Notebook/fantasy_football.py
+++ Notebook/fantasy_football.py
@@ -1,13 +1,28 @@
 import pandas as pd
 from pathlib import Path
+import numpy as np
 
 def read_fantasy_data(start_year,end_year):
     years = range(start_year,end_year+1)
     dfs=[]
     for year in years:
-        df = pd.read_csv(Path(f"../Data/RawData/{year}.csv"))
-        df["Year"]= year
-        df = df[["Year","Player","Tm","Pos","FantasyPoints"]]
-        df = df.set_index("Year")
-        dfs.append(df)
+        if year != 2020:
+            df = pd.read_csv(Path(f"../Data/RawData/{year}.csv"))
+            df["Year"]= year
+            df = df[["Year","Player","Tm","Pos","FantasyPoints"]]
+            df = df.set_index("Year")
+            dfs.append(df)
+        
+        else:
+            df = pd.read_csv(Path(f"../Data/RawData/{year}.csv"), header=1)
+            df["Year"]= year
+            df = df[["Year","Player","Tm","FantPos","PPR"]]
+            df = df.rename(columns={"FantPos": "Pos", "PPR" : "FantasyPoints"})
+            for ind in df.index:
+                df['Player'][ind] = df['Player'][ind].split(sep="\\")[0].replace("*", "").replace("+", "").strip()
+            df = df.set_index("Year")
+            dfs.append(df)
+
+            
+        
     return pd.concat(dfs)
